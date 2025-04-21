ea7d78bd01f79173051832e98eb6770aefd7d6ae
Melissa Autumn <all@melissaautumn.com>
2025-04-21 16:50:05 -0700

    Fix up the ci script

--- bundle.py
+++ bundle.py
@@ -4,21 +4,22 @@ import tomllib
 
 
 def main():
-    base_dir = 'src/io_scene_lithtech/'
+    base_dir = 'src/io_scene_lithtech'
 
-    with open('blender_manifest.toml', 'rb') as fh:
+    with open(f'{base_dir}/blender_manifest.toml', 'rb') as fh:
         config = tomllib.load(fh)
 
     id = config.get('id')
-    version = config.get('version')
 
-    zip_name = f'{id}-{version}.zip'
+    zip_name = f'{id}.zip'
 
     with ZipFile(zip_name, 'w') as extZip:
         extZip.write('LICENSE', 'io_scene_lithtech/LICENSE')
         extZip.write('README.md', 'io_scene_lithtech/README.md')
         for file in glob(f'{base_dir}/**'):
-            out_file = file.replace(base_dir, '')
+            if '__pycache__' in file:
+                continue
+            out_file = file.replace(f'{base_dir}/', '')
             extZip.write(file, f'io_scene_lithtech/{out_file}')
 
 
