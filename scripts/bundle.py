db7136f589865626393d2549b5b360ac8ad9318a
Melissa Autumn <all@melissaautumn.com>
2025-04-26 21:10:29 -0700

    Fix bundle script

--- scripts/bundle.py
+++ scripts/bundle.py
@@ -11,7 +11,7 @@ if os.getcwd().endswith('scripts'):
 def main():
     base_dir = 'src/io_scene_lithtech'
 
-    with open(f'{base_dir}/blender_manifest.toml.prod', 'rb') as fh:
+    with open(f'{base_dir}/blender_manifest.toml', 'rb') as fh:
         config = tomllib.load(fh)
 
     id = config.get('id')
@@ -24,15 +24,12 @@ def main():
         for file in glob(f'{base_dir}/**'):
             if '__pycache__' in file:
                 continue
-            # Ignore our dev manifest / local defines
-            if file.endswith('blender_manifest.toml') or file.endswith(
-                'local_defines.py'
-            ):
+
+            # Ignore our local defines
+            if file.endswith('local_defines.py'):
                 continue
+
             out_file = file.replace(f'{base_dir}/', '')
-            # Copy our prod manifest as just blender_manifest.toml
-            if file.endswith('blender_manifest.toml.prod'):
-                out_file = file.replace('.prod', '')
             extZip.write(file, f'io_scene_lithtech/{out_file}')
 
 
